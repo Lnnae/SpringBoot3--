6026aae4732987c063f192e03a5bfbaeca5c080c
Lnnae <3430537146@qq.com>
2026-06-27 15:46:33 +0800

    boot-actor
    1

---
+++ demo_actor/src/main/java/com/example/demo_actor/DemoActorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_actor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoActorApplication.class, args);
+    }
+
+}
---
+++ demo_actor/src/test/java/com/example/demo_actor/DemoActorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_actor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
